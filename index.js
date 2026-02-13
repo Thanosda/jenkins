1d00ac082933f7031ed8c59dcf4e9dd59fb5fb82
thanosda <154046417+Thanosda@users.noreply.github.com>
2026-02-13 20:57:14 +0530

    fs

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello Jenkins!');
+console.log('This is a simple Jenkins pipeline example.');
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting ");
+setTimeout(() => console.log("It will wait for 3 minutes"), 3000);
+console.log("This is a test file completed.");
